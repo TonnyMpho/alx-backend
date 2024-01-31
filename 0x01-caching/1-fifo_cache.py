013b13df3312078735bf2c36696bf39e2f68c1c1
TonnyMpho <tonnympho1@gmail.com>
2024-01-31 07:55:32 -0800

    caching

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" FIFO caching """
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    def __init__(self):
+        super().__init__()
+        self.keys = []
+    """
+    class that inherits from BaseCaching and is a caching system
+    """
+    def put(self, key, item):
+        """
+        assign to the dictionary cache_data the item value for the key key
+        If the number of items in cache_data is higher that BaseCaching.MAX_ITEMS:
+        discard the first item put in cache (FIFO algorithm)
+        print DISCARD: with the key discarded and followed by a new line
+        """
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                discard = self.keys.pop(0)
+                del self.cache_data[discard]
+                print("DISCARD: {}".format(discard))
+
+            self.cache_data[key] = item
+            self.keys.append(key)
+
+    def get(self, key):
+        """
+        return the value in self.cache_data linked to key
+        """
+        if key is not None:
+            return self.cache_data.get(key)
