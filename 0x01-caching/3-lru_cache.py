b01493b13596eb57d93fe28709ed503b73eccd42
TonnyMpho <tonnympho1@gmail.com>
2024-01-31 16:37:01 -0800

    caching

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -20,20 +20,21 @@ class LRUCache(BaseCaching):
         print DISCARD: with the key discarded and followed by a new line
         """
         if key is not None and item is not None:
-            if key in self.cache_data.keys():
-                self.keys.remove(key)
-            else:
-                if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                    discard = self.keys.pop(0)
-                    del self.cache_data[discard]
-                    print("DISCARD: {}".format(discard))
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                discard = self.keys.pop(0)
+                del self.cache_data[discard]
+                print("DISCARD: {}".format(discard))
 
-                self.cache_data[key] = item
-                self.keys.append(key)
+            self.cache_data[key] = item
+            self.keys.append(key)
 
     def get(self, key):
         """
         return the value in self.cache_data linked to key
         """
-        if key is not None:
-            return self.cache_data.get(key)
+        if key is not None and key in self.cache_data:
+            self.keys.remove(key)
+            self.keys.append(key)
+            return self.cache_data[key]
+        else:
+            return None
