e026e129778cf9b82bdc0440d42e9132f2b5ccf8
TonnyMpho <tonnympho1@gmail.com>
2024-01-31 16:44:13 -0800

    caching

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+""" MRU caching """
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    class that inherits from BaseCaching and is a caching system
+    """
+    def __init__(self):
+        super().__init__()
+        self.keys = []
+
+    def put(self, key, item):
+        """
+        assign to the dictionary cache_data the item value for the key key
+        If the number of items in cache_data is higher that
+        BaseCaching.MAX_ITEMS:
+        discard the most recently used item (MRU algorithm)
+        print DISCARD: with the key discarded and followed by a new line
+        """
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                discard = self.keys.pop()
+                del self.cache_data[discard]
+                print("DISCARD: {}".format(discard))
+
+            self.cache_data[key] = item
+            self.keys.append(key)
+
+    def get(self, key):
+        """
+        return the value in self.cache_data linked to key
+        """
+        if key is not None and key in self.cache_data:
+            self.keys.remove(key)
+            self.keys.append(key)
+            return self.cache_data[key]
+        else:
+            return None
