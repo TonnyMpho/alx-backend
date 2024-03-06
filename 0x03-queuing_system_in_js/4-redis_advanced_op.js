8971069983d9897934a9497c99e688004447bd69
TonnyMpho <tonnympho1@gmail.com>
2024-03-06 20:10:31 +0000

    Queuing System in JS

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,30 @@
+// 4. Node Redis client and advanced operations
+import { createClient, print } from 'redis';
+
+const client = createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.error(`Redis client not connected to the server: ${err.message}`);
+});
+
+// Create Hash using hset
+client.hset('HolbertonSchools', 'Portland', 50, print);
+client.hset('HolbertonSchools', 'Seattle', 80, print);
+client.hset('HolbertonSchools', 'New York', 20, print);
+client.hset('HolbertonSchools', 'Bogota', 20, print);
+client.hset('HolbertonSchools', 'Cali', 40, print);
+client.hset('HolbertonSchools', 'Paris', 2, print);
+
+// Display Hash using hgetall
+client.hgetall('HolbertonSchools', (err, result) => {
+  if (err) {
+    console.log(err);
+    throw error;
+  } else {
+    console.log(result);
+  }
+});
